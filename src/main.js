7a2746c8452e43bda99a79b9754281e70b9b69da
ryujinzz <kdo2240k@naver.com>
2023-12-26 17:33:48 +0900

    배경화면 반응형으로 바꾸기

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import "./styles/main.css";
+import { button } from "@/styles/main.module.css";
+import jsLogo from "@/assets/javascript.svg";
+import img from "@/assets/76.jpg";
+
+const app = document.querySelector("#app");
+const template = /*html*/ `
+<figure>
+<img src="${img}" alt="" />
+<figcaption>자바스크립트 로고</figcaption>
+</figure>`;
+
+app.insertAdjacentHTML("beforeend", template);
